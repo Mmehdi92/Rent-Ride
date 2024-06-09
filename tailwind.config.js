b65ca05220040e609e9782a1b97f87b391385bb2
Mehdi Zymberi <m.zymberi@student.avans.nl>
2024-06-09 11:25:11 +0200

    Update path  in tailwind.config & package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/views/*.{html,js,php}",
-    "./public/views/**/*.{html,js,php}",
-    "./public/*.{html,js,php}",
+    "App/public/views/*.{html,js,php}",
+    "App/public/views/**/*.{html,js,php}",
+    "App/public/*.{html,js,php}",
   ],
   theme: {
     extend: {},
